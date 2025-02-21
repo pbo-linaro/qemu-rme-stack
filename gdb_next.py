182f60217eaad83ede6977951dcef9ec175bf1e9
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-02-20 20:23:05 -0800

    gdb_next: turn methods into functions

--- gdb_next.py
+++ gdb_next.py
@@ -1,6 +1,18 @@
 import gdb
 import time
 
+def current_binary():
+    try:
+        return gdb.selected_frame().find_sal().symtab.objfile.filename
+    except:
+        return ''
+
+def current_source():
+    try:
+        return gdb.selected_frame().find_sal().symtab.fullname()
+    except:
+        return ''
+
 def step_instruction_until_state_change(get_state):
     from_state = get_state()
     while True:
@@ -16,28 +28,16 @@ class next_binary(gdb.Command):
     def __init__(self):
         super(next_binary, self).__init__("next-binary", gdb.COMMAND_USER)
 
-    def current_binary(self):
-        try:
-            return gdb.selected_frame().find_sal().symtab.objfile.filename
-        except:
-            return ''
-
     def invoke(self, argument, fromtty):
-        step_instruction_until_state_change(self.current_binary)
+        step_instruction_until_state_change(current_binary)
 
 # Single step until we reach a new source file
 class next_source(gdb.Command):
     def __init__(self):
         super(next_source, self).__init__("next-source", gdb.COMMAND_USER)
 
-    def current_source(self):
-        try:
-            return gdb.selected_frame().find_sal().symtab.fullname()
-        except:
-            return ''
-
     def invoke(self, argument, fromtty):
-        step_instruction_until_state_change(self.current_source)
+        step_instruction_until_state_change(current_source)
 
 next_binary()
 next_source()
